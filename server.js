3b39a7f0c15057d28ae59abdc1c6b93099380c7d
Kush Shah <kush@unc.edu>
2022-11-27 20:46:04 -0500

    rename index and fill out app.get, http require

---
+++ server.js
@@ -0,0 +1,21 @@
+// require express
+const express = require('express');
+const app = express();
+
+// require http module
+const http = require('http');
+
+// require minimist and process arguments
+const args = require('minimist')(process.argv.slice(2));
+
+// set port to 5000 if no argument is given
+const port = args.port || 5000;
+
+
+app.get('/', (req, res, next) => {
+
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+});
